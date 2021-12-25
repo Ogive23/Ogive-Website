05b6c08d8d8b0e818ecc534f96f38e160e6d01fc
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-25 18:19:28 +0200

    Added Initial Brief Component

--- src/Components/Brief.js
+++ src/Components/Brief.js
@@ -1,5 +1,55 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
 export const Brief = () => {
   return (
+    <Box
+    sx={{
+        backgroundPosition: "top right",
+        // height: "70vh",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        // position: "relative",
+        backgroundImage:
+          'url("https://www.analyticabi.app/Frontend/Visitor/HomePage/Images/landing-bg2.svg")',
+        marginRight: "0",
+        marginLeft: "0",
+        overflow: "auto",
+        marginTop: "0px",
+      }}
+    >
+      <Box
+        sx={{
+          height: "70vh",
+          position: "relative",
+          marginRight: "0",
+          marginLeft: "0",
+          overflow: "auto",
+          width: "100%",
+          marginTop: "10vh",
+          marginBottom: "auto",
+        }}
+      >
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            mr: "35%",
+            display: { md: "flex" },
+            marginLeft: "5%",
+            marginTop: "auto",
+            marginBottom: "auto",
+            height: "inherit",
+            alignItems: "center",
+          }}
+        >
+          Ogive is In publishing and graphic design, Lorem ipsum is a
+          placeholder text commonly used to demonstrate the visual form of a
+          document or a typeface without relying on meaningful content. Lorem
+          ipsum may be used as a placeholder before the final copy is available.
+        </Typography>
+      </Box>
+    </Box>
   );
 };
