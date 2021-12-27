90e871dbac2868768fa7e1c2b6d3f49be8be328e
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-27 15:44:11 +0200

    Update Projects Component

--- src/Components/AnimatedProjectImages.js
+++ src/Components/AnimatedProjectImages.js
@@ -12,22 +12,20 @@ const cards = [
     'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'
 ]
 
-// These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = i => ({ x: 0, y: i * -4, scale: 1, display: 'auto', rot: -10 + Math.random() * 20, delay: i * 100 })
-const from = i => ({ x: 0, rot: 0, scale: 1.5, display: 'auto', y: -1000 })
+const to = i => ({ x: 0, y: i * -4, scale: 1, display: 'block', rot: -10 + Math.random() * 20, delay: i * 100 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
-
 
 export const AnimatedProjectImages = () => {
     const [width, setWidth] = useState(window.innerWidth);
     const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
-    const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
+    const [props, set] = useSprings(cards.length, i => ({ ...to(i) })) // Create a bunch of springs using the helpers above
     // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-    const bind = useGesture(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
-        console.log(xDelta);
+    const bind = useGesture(({ args: [index], down, delta: [xDelta], direction: [xDir], velocity }) => {
+
         const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
-        const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+        // const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+        const dir = 1;
         if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
         set(i => {
             if (index !== i) return // We're only interested in changing spring-data for the current spring
@@ -35,10 +33,10 @@ export const AnimatedProjectImages = () => {
             const x = isGone ? (width / 2) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
             const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
             const scale = down ? 1.1 : 1 // Active cards lift up a bit
-            const display = down ? 'none' : 'auto'
+            const display = isGone ? 'none' : 'block'
             return { x, rot, scale, display, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
         })
-        if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
+        if (gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
     })
 
 
@@ -53,13 +51,15 @@ export const AnimatedProjectImages = () => {
     });
 
     // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-    // console.log(trans);
-
     return props.map(({ x, y, rot, scale, display }, i) => {
         return (
             <animated.div className="parent" key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
                 {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-                <animated.div className="child" {...bind(i)} style={{ display: display, transform: interpolate([rot, scale], trans), backgroundImage: `url(${cards[i]})` }} />
+                <animated.div className="child" {...bind(i)} style={{
+                    display: display,
+                    transform: interpolate([rot, scale], trans),
+                    backgroundImage: `url(${cards[i]})`
+                }} />
             </animated.div>
         );
     });
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,17 +1,78 @@
-import React from 'react'
-import { AnimatedProjectImages } from './AnimatedProjectImages';
+import { Box, Grid, Paper, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import ReactCardFlip from 'react-card-flip';
 
 export const Projects = () => {
-    
-    return (
-        <div style={{ 
-            width: '45vh',
-            height: '25vh',
-            marginLeft: 'auto',
-            marginRight : 'auto'
-         }}>
+    const [cards, setCards] = useState([
+        {
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1280px-24701-nature-natural-beauty.jpg',
+            isFlipped: false,
+        }, {
+            image: 'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg',
+            isFlipped: false,
+        }, {
+            image: 'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg',
+            isFlipped: false,
+        }, {
+            image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
+            isFlipped: false,
+        }, {
+            image: 'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
+            isFlipped: false,
+        }])
+    const handleFlipClick = (index) => {
+        let updatedCards = [...cards];
+        updatedCards[index].isFlipped = !updatedCards[index].isFlipped;
+        setCards(updatedCards);
+    }
 
-<AnimatedProjectImages />
-        </div>
+    return (
+        <Box sx={{
+            backgroundImage: 'url(https://d1l5pp53ux74mz.cloudfront.net/images/default-source/backgrounds/brand-refresh-q4-2021/home_hero_top_element.svg.svg?sfvrsn=6c39ecde_6)',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'bottom',
+            backgroundSize: 'cover',
+            // display: 'flex'
+        }}>
+            <Typography sx={{
+                marginLeft: '3vw', marginTop: '0%',
+                // position: 'absolute',
+                paddingTop: '3%',
+                transform: 'rotate(335deg)',
+                width: 'fit-content'
+            }} color="#1B545D" variant="h3" component="h2">
+                Projects
+            </Typography>
+            <Grid container spacing={2} sx={{ paddingTop: '5%' }}>
+                {
+                    cards.map((card, index) => {
+                        console.log(card);
+                        return <Grid item xs={4}>
+                            <ReactCardFlip isFlipped={card.isFlipped} flipDirection="vertical" cardStyles={{ height: '30vh' }} containerStyle={{ height: '30vh' }} >
+                                <Box height={'30vh'} sx={{
+                                    backgroundImage: 'url(' + card.image + ')',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundPosition: 'center',
+                                    backgroundSize: 'contain'
+                                }} onClick={() => handleFlipClick(index)} >
+                                </Box>
+                                <Box height={'20vh'} sx={{ textAlign: 'center', background: 'white' }} onClick={() => handleFlipClick(index)} >
+                                    This is the back of the card.
+                                </Box>
+                            </ReactCardFlip>
+                        </Grid>;
+                    })
+                }
+            </Grid>
+        </Box>
     )
+    {/* <Grid item xs={4} sx={{
+                width: '45vh',
+                height: '25vh',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                overflow: 'hidden'
+            }}> */}
+    {/* <AnimatedProjectImages /> */ }
+    {/* </Grid> */ }
 }
